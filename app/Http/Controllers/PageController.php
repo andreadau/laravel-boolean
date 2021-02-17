39b5949adfe19102577ff9e04c2d527b6275f93c
Andrea <dauriaandrea99@gmail.com>
2021-02-17 16:09:19 +0100

    Add Route and Db Call

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    /**
+     * Show index page
+     *  @return view
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+      /**
+     * Show about page
+     *  @return view
+     */
+    public function about()
+    {
+        return view('about');
+    }
+      /**
+     * Show post page
+     *  @return view
+     */
+    public function blog(Post $post)
+    {
+        $posts = $post->all();
+        var_dump($posts);
+        return view('blog', compact($posts));
+    }
+}
