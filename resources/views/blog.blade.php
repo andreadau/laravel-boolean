db10a89069a03e1c78e5e4b19ae46f584726c40e
Andrea <dauriaandrea99@gmail.com>
2021-02-17 16:52:15 +0100

    End Route Call !END!

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout.app')
+@section('main')
     <main>
         @foreach($posts as $post)
         <h2>{{$post->title}}</h2>
         <p>{{$post->description}}</p>
         @endforeach
     </main>
-</body>
-</html>
+@endsection
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="content">
+            <div class="title m-b-md">
+                Laravel Boolean
+            </div>
+            <div class="links">
+                    <a href="/">Home</a>
+                <a href="about">about</a>
+                <a href="blog">Blog</a>
+        </div>
+    </div>
+</header>
